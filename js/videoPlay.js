c138746a58c798c4f6ebf0987ab0fa6010eb2e39
LevaMerzlyak <13dir13@gmail.com>
2018-04-24 16:57:01 +0300

    test3

--- js/videoPlay.js
+++ js/videoPlay.js
@@ -7,7 +7,7 @@ function VideoPlay(sSelector) {
 	v.container = $('.videoAbout__wrap');
 	v.playerWrap = v.container.find('.video__wrap')
 	v.player = v.container.find('#video1');
-	v.videoPlayer = videojs('#video1');
+	//v.videoPlayer = videojs('#video1');
 
 	v.closeBtn = v.container.find('.btn_videoClose');
 
